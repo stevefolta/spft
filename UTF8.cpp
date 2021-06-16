7bb687c8d80519dbaf69a92baa65ac6cddbdbba3
Steve Folta <steve@folta.net>
2021-06-16 00:33:02 -0700

    Fixed and simplified UTF8::num_characters().

--- UTF8.cpp
+++ UTF8.cpp
@@ -5,36 +5,13 @@
 int UTF8::num_characters(const char* bytes, int length)
 {
 	// Assumes valid UTF8.
-	const char* p = bytes;
-	const char* end = bytes + length;
+	const unsigned char* p = (const unsigned char*) bytes;
+	const unsigned char* end = p + length;
 	int num_chars = 0;
-	while (p < end) {
-		unsigned char c = *p++;
-		if (c < 0x80)
+	for (; p < end; ++p) {
+		unsigned char c = *p;
+		if (c < 0x80 || c >= 0xC0)
 			num_chars += 1;
-		else if ((c & 0xFD) == 0xFC) {
-			num_chars += 6;
-			p += 5;
-			}
-		else if ((c & 0xFC) == 0xF8) {
-			num_chars += 5;
-			p += 4;
-			}
-		else if ((c & 0xF8) == 0xF0) {
-			num_chars += 4;
-			p += 3;
-			}
-		else if ((c & 0xF0) == 0xE0) {
-			num_chars += 3;
-			p += 3;
-			}
-		else if ((c & 0xE0) == 0xC0) {
-			num_chars += 2;
-			p += 1;
-			}
-		else {
-			// Invalid, but we don't handle that.
-			}
 		}
 	return num_chars;
 }
